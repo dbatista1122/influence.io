be8f4cde701da1d73f8c125f6ac07228e9650c18
Diego Batista <batistadiego01@gmail.com>
2023-09-25 14:32:25 -0400

    revert

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,9 +5,8 @@ var router = express.Router();
 /* GET users async req. */
 router.get('/getUsers', async(req, res) => {
     try {
-        // var result = await db.query('SELECT * FROM users');
-        // res.send(result.rows);
-        res.render("HELLO");
+        var result = await db.query('SELECT * FROM users');
+        res.send(result.rows);
     }
     catch {
         res.status(400).json("Unsuccessful Request");
