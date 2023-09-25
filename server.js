728faf3eba1d93941128884d7aebd354310ac7c0
Diego Batista <batistadiego01@gmail.com>
2023-09-25 18:04:57 -0400

    test

--- server.js
+++ server.js
@@ -1,29 +1,35 @@
-const express = require('express')
-const next = require('next')
-require('dotenv').config
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
+const pathMatch = require('path-match');
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const { parse } = require('url');
 
-const userApi = require('./server/routes/users')
+const userRoutes = require('./server/routes/users.js');
 
-app.prepare()
-.then(() => {
-  const server = express()
+app.prepare().then(() => {
+  const server = express();
 
-  server.use('/users', userApi)
+  server.use(bodyParser.json());
+  server.use(session({
+    secret: 'super-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }
+  }));
+
+  server.use('/users', userRoutes);
 
   server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+    return handle(req, res);
+  });
 
   server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+    if (err) throw err;
+    console.log('Server ready on http://localhost:3000');
+  });
+});
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,17 +2,8 @@ var express = require('express');
 const db = require('../../db/db');
 var router = express.Router();
 
-/* GET users async req. */
-router.get('/getUsers', async(req, res) => {
-    try {
-        var result = await db.query('SELECT * FROM users');
-        res.send(result.rows);
-    }
-    catch {
-        res.status(400).json("Unsuccessful Request");
-    }
-
+router.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the API!' });
 });
-
 
 module.exports = router;
