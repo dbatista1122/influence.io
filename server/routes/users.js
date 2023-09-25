6bd9512a82604f6289e8c002f239866bafc28a66
Diego Batista <batistadiego01@gmail.com>
2023-09-25 14:24:41 -0400

    test getUsers

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,8 +5,9 @@ var router = express.Router();
 /* GET users async req. */
 router.get('/getUsers', async(req, res) => {
     try {
-        var result = await db.query('SELECT * FROM users');
-        res.send(result.rows);
+        // var result = await db.query('SELECT * FROM users');
+        // res.send(result.rows);
+        res.render("HELLO");
     }
     catch {
         res.status(400).json("Unsuccessful Request");
