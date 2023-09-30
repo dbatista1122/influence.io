3367802df308fc1d106c407075284241544e0d84
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-09-30 19:23:55 -0400

    Separated navbar functionality to separate functions, separated logo to separate component.

--- components/ProfileFooter.js
+++ components/ProfileFooter.js
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
+import Logo from "./Logo";
 
 const ProfileFooter = () => {
   return (
     <footer className="flex items-center justify-between">
+      <Logo />
       <Link href="/" className="flex gap-4 items-center">
-        <Image width={36} height={36} src="/favicon.ico" className="w-8 md:w-9" alt="logo" />{" "}
         <span>Influence.io</span>
       </Link>
 
