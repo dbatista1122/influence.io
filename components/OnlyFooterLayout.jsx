b0798556b9d35ac470e4ee33f2b1f2105a527987
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-09-30 23:09:10 -0400

    changed title for onlyfooterlayout, still need to figure out a way for register/login to change title

--- components/OnlyFooterLayout.jsx
+++ components/OnlyFooterLayout.jsx
@@ -1,13 +1,20 @@
 import ProfileFooter from "./ProfileFooter";
+import Head from "next/head";
 
 const OnlyFooterLayout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen mx-auto max-w-4xl">
-      <div className="flex-grow">
-        <main>{children}</main>
+    <>
+      <Head>
+        <title>Influence.io</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col min-h-screen mx-auto max-w-4xl">
+        <div className="flex-grow">
+          <main>{children}</main>
+        </div>
+        <ProfileFooter />
       </div>
-      <ProfileFooter />
-    </div>
+    </>
   );
 };
 
