675567689d0a3cd150f5e5612a50c74385ca1caa
Diego Batista <batistadiego01@gmail.com>
2023-11-10 17:14:28 -0500

    youtube api updates fetching data

--- pages/dashboard/youtube.jsx
+++ pages/dashboard/youtube.jsx
@@ -1,106 +1,124 @@
-import Link from "next/link";
 import RootLayout from "@/components/Layout";
 import DashboardLayout from "@/components/DashboardLayout";
-import { FaGoogle, FaFacebookF, FaTwitter, FaRegEnvelope } from "react-icons/fa";
-import { MdLockOutline } from "react-icons/md";
-import React from 'react'
-import { GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
+import { FaGoogle } from "react-icons/fa";
+import React, { useState } from 'react'
+import { GoogleOAuthProvider, useGoogleLogin, googleLogout } from '@react-oauth/google';
 
 function Analytics() {
 
-  async function handleSubmit(credentialResponse) {
-    console.log(credentialResponse);
+  const [hasGoogleClient, setHasGoogleClient] = useState(false);
+  const [accessToken, setAccessToken] = useState("");
 
-
-    async function getData() {
-      const statsData = fetchData(statisticsURL);
-      const uploadsData = fetchData(uploadsURL);
-  
-      return {
-        stats: await statsData,
-        videos: await uploadsData,
-      };
-    }
-  }
-  const hasGoogleClient = true;
-
-  return(
+  return (
     <RootLayout>
       <DashboardLayout>
-        <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
-
-        {hasGoogleClient && (
-            <YoutubeLoginButton />      
-        )}
-        {!hasGoogleClient && (
-          <>
-            Show Data Here
-          </>
-        )}     
-      </GoogleOAuthProvider>
-    </DashboardLayout>
-  </RootLayout>
-
+        <GoogleOAuthProvider clientId="516636552-eemg5hgaqg6ms45n1lcfdmvvvrq4dhlp.apps.googleusercontent.com">
+          {!hasGoogleClient ? (
+              <YoutubeLoginButton 
+              setHasGoogleClient={setHasGoogleClient} 
+              setAccessToken={setAccessToken}/>
+          ) : (
+            <YoutubeAnalytics
+             setHasGoogleClient={setHasGoogleClient} 
+             accessToken={accessToken}/>
+          )}
+        </GoogleOAuthProvider>
+      </DashboardLayout>
+    </RootLayout>
   );
-};
+}
 
-function YoutubeLoginButton() {
+function YoutubeLoginButton({ setHasGoogleClient, setAccessToken }) {
+  
+  async function handleSuccess(codeResponse, setAccessToken, setHasGoogleClient) {
+    const access_token = codeResponse.access_token
+    setAccessToken(String(access_token))
+    setHasGoogleClient(true)
+  }
 
   const login = useGoogleLogin({
-    onSuccess: codeResponse => console.log(codeResponse),
-    scope: "https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly https://www.googleapis.com/auth/youtube",  
+    onSuccess: async (codeResponse) => await handleSuccess(codeResponse, setAccessToken, setHasGoogleClient),
+    scope: "https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/yt-analytics.readonly https://www.googleapis.com/auth/yt-analytics-monetary.readonly https://www.googleapis.com/auth/youtube",  
   });
 
   return (
     <div className="flex flex-col p-5 items-center">
       <div className=" p-5 items-center">
-            <button  onClick={() => login()} className="flex flex-row gap-3 item-center border-2 rounded-full text-sm px-5 py-2 inline-block hover:bg-gray-600 hover:text-white">
-              <div>Add YouTube Account</div>
-              <FaGoogle className="text-lg" />
-            </button >
-        </div>
+        <button onClick={() => login()} className="flex flex-row gap-3 item-center border-2 rounded-full text-sm px-5 py-2 inline-block hover-bg-gray-600 hover-text-white">
+          <div>Add YouTube Account</div>
+          <FaGoogle className="text-lg" />
+        </button>
+      </div>
     </div>
-  
   );
 }
 
-function AccountInput({ email, password, setEmailAddress, setPassword, onSubmit }) {
+function YoutubeAnalytics({ setHasGoogleClient, accessToken }) {
+
+  function handleLogout() {
+    setHasGoogleClient(false); // Set the user as logged out
+    googleLogout();
+  }
+
+  async function getChannelId() {
+    
+    const res = await fetch("/api/youtube/getChannelId", {
+      method: 'POST',
+      body: JSON.stringify({
+        bearerToken: accessToken
+      })
+    });
+
+    const response = await res.json()
+    const channelId = response.data.items[0].id
+    
+    return channelId
+
+  }
+
+
+  async function loadChannelData() {
+    const channel = await getChannelId()
+    
+    const res = await fetch("/api/youtube/getChannelData", {
+      method: 'POST',
+      body: JSON.stringify({
+        channel: channel
+      })
+    });
+
+    const response = await res.json()
+    
+    const channelName = response.data.items[0].snippet.title;
+    const viewCount = response.data.items[0].statistics.viewCount;
+    const subscriberCount = response.data.items[0].statistics.subscriberCount;
+    const videoCount = response.data.items[0].statistics.videoCount;
+
+    const customUrl = response.data.items[0].snippet.customUrl;
+    const channelUrl = `https://www.youtube.com/${customUrl}`;
+
+    // Output the results
+    console.log("Channel Name:", channelName);
+    console.log("View Count:", viewCount);
+    console.log("Subscriber Count:", subscriberCount);
+    console.log("Video Count:", videoCount);
+    console.log("Channel URL:", channelUrl);
+
+  }
+
   return (
-    <form onSubmit={onSubmit}>
-      <div className="flex flex-col p-5 items-center">
-        <div className="bg-gray-100 w-64 flex items-center border-2 rounded-full mb-5">
-          <FaRegEnvelope className="text-gray-400 m-2" />
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="bg-gray-100 outline-none text-sm flex-1"
-            value={email}
-            onChange={(evnt) => setEmailAddress(evnt.target.value)}
-            required
-          />
-        </div>
-
-        <div className="bg-gray-100 w-64 flex items-center border-2 rounded-full mb-5">
-          <MdLockOutline className="text-gray-400 m-2" />
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="bg-gray-100 outline-none text-sm flex-1"
-            value={password}
-            onChange={(evnt) => setPassword(evnt.target.value)}
-            required
-          />
-        </div>
-
-        <button className="flex gap-3 item-center border-2 rounded-full text-sm px-5 py-2 inline-block hover:bg-gray-600 hover:text-white">
-          <div>Add YouTube Account</div>
-          <FaGoogle className="text-sm" />
+    <div className="flex flex-col p-5 items-center">
+      <div className=" p-5 items-center">
+        Youtube analytics
+        <button onClick={loadChannelData} className="flex flex-row gap-3 item-center border-2 rounded-full text-sm px-5 py-2 inline-block hover-bg-gray-600 hover-text-white">
+          <div>LoadData</div>
         </button>
-
+        <button onClick={handleLogout} className="flex flex-row gap-3 item-center border-2 rounded-full text-sm px-5 py-2 inline-block hover-bg-gray-600 hover-text-white">
+          <div>Logout</div>
+          <FaGoogle className="text-lg" />
+        </button>
       </div>
-    </form>
+    </div>
   );
 }
 
