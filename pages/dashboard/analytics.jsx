5aac6c93cc5393eedf89e220649c075ac6ded98b
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-11-16 04:59:43 -0500

    added react chart.js

--- pages/dashboard/analytics.jsx
+++ pages/dashboard/analytics.jsx
@@ -1,15 +1,15 @@
 import RootLayout from "@/components/Layout";
 import DashboardLayout from "@/components/DashboardLayout";
-import { signOut, useSession } from 'next-auth/react';
-
+import { signOut, useSession } from "next-auth/react";
 
 const Analytics = () => {
-
   const session = useSession();
-  return <div>
-    General info/account connection here... {session.status.data}
-    {/* <button onClick={() => signOut({callbackUrl: "/"})}>Sign Out</button> */}
-    </div>;
+  return (
+    <div>
+      General info/account connection here... {session.status.data}
+      {/* <button onClick={() => signOut({callbackUrl: "/"})}>Sign Out</button> */}
+    </div>
+  );
 };
 
 Analytics.getLayout = (page) => (
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,37 +1,35 @@
 import RootLayout from "@/components/Layout";
 import Image from "next/image";
 
-
 const Home = () => {
   return (
     <main>
       <div className="flex flex-col items-center ">
         <h1 className="text-4xl font-bold mb-4 pt-10">Welcome to Influence.io</h1>
-        <p className="text-2xl">
-          Empower Your Social Media Strategy
-        </p>
+        <p className="text-2xl">Empower Your Social Media Strategy</p>
 
         <p className="text-2lg px-40 pt-10 ">
-          At Influence.io, we've revolutionized social media management, 
-          making it simpler and more effective than ever before. 
-          Our platform offers a wide range of benefits to enhance your 
-          social media data analytics experience.          
+          At Influence.io, we've revolutionized social media management, making it simpler and more
+          effective than ever before. Our platform offers a wide range of benefits to enhance your
+          social media data analytics experience.
         </p>
       </div>
-      
+
       <section className="m-14 px-10 bg-red">
         <div className="flex items-center p-5 py-15 space-x-10">
           <Image
             className="basis-1/2"
             src="/static/ComprehensiveAnalytics.webp"
             width={5}
-            height={5}  
-            unoptimized          
-            />
+            height={5}
+            unoptimized
+          />
           <div>
-            <h2 className="basis-1/2 text-xl font-semibold mt-2 p-5 basis-1/2">Comprehensive Analytics</h2>
+            <h2 className="basis-1/2 text-xl font-semibold mt-2 p-5">Comprehensive Analytics</h2>
             <p>
-              Gain deep insights into your social media performance. Measure engagement, track growth, and uncover what content resonates with your audience.            </p>
+              Gain deep insights into your social media performance. Measure engagement, track
+              growth, and uncover what content resonates with your audience.{" "}
+            </p>
           </div>
         </div>
 
@@ -39,7 +37,9 @@ const Home = () => {
           <div>
             <h2 className="basis-1/2 text-xl font-semibold p-5 mt-2">Multi-Platform Integration</h2>
             <p>
-            Manage multiple social media accounts in one place. Whether it's Facebook, Twitter, Instagram, or LinkedIn, we've got you covered.            </p>
+              Manage multiple social media accounts in one place. Whether it's Facebook, Twitter,
+              Instagram, or LinkedIn, we've got you covered.{" "}
+            </p>
           </div>
           <Image
             className="basis-1/2"
@@ -48,7 +48,6 @@ const Home = () => {
             height={5}
             unoptimized
           />
-        
         </div>
 
         <div className="flex items-center p-5 py-15 justify-center space-x-10">
@@ -63,12 +62,13 @@ const Home = () => {
           <div>
             <h2 className="basis-1/2 text-xl font-semibold mt-2 p-5 item-center">Data Security</h2>
             <p>
-            Rest easy knowing your data is protected. We employ the highest security standards to keep your information safe and secure.            </p>
+              Rest easy knowing your data is protected. We employ the highest security standards to
+              keep your information safe and secure.{" "}
+            </p>
           </div>
         </div>
       </section>
-
-  </main>
+    </main>
   );
 };
 
