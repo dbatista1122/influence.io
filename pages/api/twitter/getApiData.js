85dd59d5202116a2bd514dc0d8c01f3d6acc1f4e
Chidiogo Onoh <chidi.onoh@gmail.com>
2023-12-06 12:24:39 -0500

    idk this works too

--- pages/api/twitter/getApiData.js
+++ pages/api/twitter/getApiData.js
@@ -17,10 +17,33 @@ export async function getApiData(access_token) {
         'tweet.fields': 'public_metrics'
       })
       console.log(tweetMetrics);
-    return {user, tweetMetrics};
+      /*
+      const responseData = {
+        retweet_count: tweetMetrics.data[0].public_metrics.retweet_count,
+        reply_count: tweetMetrics.data[0].public_metrics.reply_count,
+        like_count: tweetMetrics.data[0].public_metrics.like_count,
+        impression_count: tweetMetrics.data[0].public_metrics.impression_count,
+        // Add other relevant data as needed
+      };
+      return responseData;
+      */
+     return {user, tweetMetrics};
   } catch (error) {
     // Handle errors
     console.error('Error in Twitter API call:', error);
   }
 
 }
+/*
+export default async function handler(req, res) {
+    // Your existing code for the API route
+    try {
+      const access_token = req.body.accessToken;
+      const result = await getApiData(access_token);
+      res.json(result);
+    } catch (error) {
+      console.error('Error in API route:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+*/
